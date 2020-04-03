c055832732ca4b78667cfee4998b0650065999cf
Hasmiou <diallowansan@gmail.com>
2020-04-03 15:43:45 +0200

    first commit

---
+++ src/test/java/com/mondemarcheur/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mondemarcheur.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
