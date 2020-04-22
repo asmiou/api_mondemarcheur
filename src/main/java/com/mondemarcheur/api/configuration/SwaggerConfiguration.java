2155d254ddfbfde96b60a8808ce88500f6c0a59c
Hasmiou <diallowansan@gmail.com>
2020-04-22 06:16:58 +0200

    Update: Optimize swagger iu

--- src/main/java/com/mondemarcheur/api/configuration/SwaggerConfiguration.java
+++ src/main/java/com/mondemarcheur/api/configuration/SwaggerConfiguration.java
@@ -8,14 +8,19 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.service.AuthorizationScope;
 
 import java.awt.print.Pageable;
 import java.util.Date;
+import java.util.List;
 
 @EnableSwagger2WebMvc
 @Import(SpringDataRestConfiguration.class)
@@ -71,4 +76,5 @@ public class SwaggerConfiguration {
                 .version("1.0.0")
                 .build();
     }
+    
 }
